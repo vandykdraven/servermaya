f092e06657610dc137978bbda8bccf922721861f
vandykdraven <42025316+vandykdraven@users.noreply.github.com>
2018-12-31 13:22:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -310,7 +310,7 @@
     <div class="container">
         <div class="media-container-row align-center mbr-white">
             <div class="col-12">
-                <p class="mbr-text mb-0 mbr-fonts-style display-7">© Copyright <? echo date('Y'); ?> <a href="http://servermaya.web.id">ServerMaya </a>- All Rights Reserved</p>
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">© Copyright 2017 <a href="http://servermaya.web.id">ServerMaya </a>- All Rights Reserved</p>
             </div>
         </div>
     </div>
